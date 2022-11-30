27652be7be4cba8923cfea2a9fb648c65e0bd958
Dicksonrsg <dickson.el@gmail.com>
2022-11-30 16:33:06 -0300

    Adiciona constantes

--- SenhaCaixa/fila_base.py
+++ SenhaCaixa/fila_base.py
@@ -1,11 +1,37 @@
-class FilaBase:
+import abc
+
+from constantes import TAMANHO_PADRAO_MAXIMO, TAMANHO_PADRAO_MINIMO
+
+
+class FilaBase(metaclass= abc.ABCMeta):
     codigo: int = 0
     fila = []
     clientes_atendidos = []
     senha_atual: str = ""
 
     def reseta_fila(self) -> None:
-        if self.codigo >= 200:
-            self.codigo = 0
+        if self.codigo >= TAMANHO_PADRAO_MAXIMO:
+            self.codigo = TAMANHO_PADRAO_MINIMO
         else:
             self.codigo += 1
+
+    def inseri_cliente(self) -> None:
+        self.fila.append(self.senha_atual)
+
+    def gera_senhas(self):
+        self.busca_posicao_fila()
+        self.gera_senha_atual()
+        self.inseri_cliente()
+
+    @abc.abstractmethod
+    def chama_cliente(self, caixa):
+        ...
+
+    @abc.abstractmethod
+    def estatistica(self, dia, agencia, flag_detail):
+        ...
+
+    @abc.abstractmethod
+    def atualiza_fila(self):
+        ...
+    
