f83be7b356a0720382917f1a9424e43431f8cf1a
Dicksonrsg <dickson.el@gmail.com>
2022-12-01 06:42:00 -0300

    Updates

--- SenhaCaixa/fila_base.py
+++ SenhaCaixa/fila_base.py
@@ -1,12 +1,14 @@
 import abc
 
+from typing import List
+
 from constantes import TAMANHO_PADRAO_MAXIMO, TAMANHO_PADRAO_MINIMO
 
 
 class FilaBase(metaclass= abc.ABCMeta):
     codigo: int = 0
-    fila = []
-    clientes_atendidos = []
+    fila: List[str] = []
+    clientes_atendidos: List[str] = []
     senha_atual: str = ""
 
     def reseta_fila(self) -> None:
@@ -14,6 +16,11 @@ class FilaBase(metaclass= abc.ABCMeta):
             self.codigo = TAMANHO_PADRAO_MINIMO
         else:
             self.codigo += 1
+
+    def atualiza_fila(self) -> None:
+        self.reseta_fila()
+        self.gera_senha_atual()
+        self.inseri_cliente()
 
     def inseri_cliente(self) -> None:
         self.fila.append(self.senha_atual)
---
+++ SenhaCaixa/main.py
@@ -0,0 +1,8 @@
+from fabrica_fila import FabricaFila
+
+
+teste_fabrica = FabricaFila.pega_fila('normal')
+teste_fabrica.atualiza_fila()
+teste_fabrica.atualiza_fila()
+teste_fabrica.atualiza_fila()
+print(teste_fabrica.chama_cliente(7))
